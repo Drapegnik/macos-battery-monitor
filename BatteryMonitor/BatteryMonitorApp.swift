105d48b27518920c807aed2c5ebbbe1197c21b2c
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2024-12-29 01:48:06 +0100

    Initial Commit

---
+++ BatteryMonitor/BatteryMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryMonitorApp.swift
+//  BatteryMonitor
+//
+//  Created by Ivan Pazhitnykh on 29.12.24.
+//
+
+import SwiftUI
+
+@main
+struct BatteryMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryMonitor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BatteryMonitor
+//
+//  Created by Ivan Pazhitnykh on 29.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
