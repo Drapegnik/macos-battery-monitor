1918337976d928041b6786b7ba262d99aaf9b88e
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2024-12-29 01:51:43 +0100

    feat: add hello-world tray app

--- BatteryMonitor/BatteryMonitorApp.swift
+++ BatteryMonitor/BatteryMonitorApp.swift
@@ -5,13 +5,45 @@
 //  Created by Ivan Pazhitnykh on 29.12.24.
 //
 
-import SwiftUI
+import Cocoa
 
 @main
-struct BatteryMonitorApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct BatteryMonitorApp {
+    static func main() {
+        let delegate = AppDelegate()
+        NSApplication.shared.delegate = delegate
+        NSApplication.shared.run()
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        createStatusItem()
+    }
+    
+    func createStatusItem() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        
+        if let button = statusItem?.button {
+            button.image = NSImage(systemSymbolName: "star.fill", accessibilityDescription: "Star")
+            button.action = #selector(showMenu)
         }
+    }
+    
+    @objc func showMenu() {
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Hello World", action: #selector(sayHello), keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem?.menu = menu
+    }
+    
+    @objc func sayHello() {
+        let alert = NSAlert()
+        alert.messageText = "Hello World!"
+        alert.runModal()
     }
 }
